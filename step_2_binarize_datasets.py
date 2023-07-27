5e19f6a5285afef63c8aa8ab2ad216b30552bc28
Tim Huisman <T.J.Huisman-1@student.tudelft.nl>
2023-07-27 12:10:16 +0200

    Add conversion to numerical datasets (for CTree)

--- step_2_binarize_datasets.py
+++ step_2_binarize_datasets.py
@@ -1,113 +1,185 @@
-import numpy as np
 import os
+import shutil
 from utils import files_in_directory, parse_line
 
 DIRECTORY = os.path.realpath(os.path.dirname(__file__))
-INPUT_DIRECTORY = f"{DIRECTORY}/datasets"
-OUTPUT_DIRECTORY = f"{DIRECTORY}/streed2/data/survival-analysis"
-METADATA_DIRECTORY = f"{DIRECTORY}/metadata"
+ORIGINAL_DIRECTORY = f"{DIRECTORY}/datasets/original"
+NUMERIC_DIRECTORY = f"{DIRECTORY}/datasets/numeric"
+BINARY_DIRECTORY = f"{DIRECTORY}/datasets/binary"
 
 MAX_BINARIZATIONS_PER_FEATURE = 10
 
-if __name__ == "__main__":
-    # Remove all binarized datasets in STreeD
-    for filename in files_in_directory(OUTPUT_DIRECTORY):
-        os.remove(f"{OUTPUT_DIRECTORY}/{filename}")
+def remove_redundant_binary_features(feature_names, instances):
+    new_feature_names = ["time", "event"]
+    new_instances = [j[:2] for j in instances]
 
-    for input_filename in files_in_directory(INPUT_DIRECTORY):
+    seen = set([
+        "0" * (len(instances[0]) - 1)
+    ])
+
+    for i in range(2, len(instances[0])):
+        values = [inst[i] for inst in instances]
+
+        if len(set(values) - set([0, 1])) == 0:
+            key = "".join(str(inst[i]) for inst in instances)
+            complement_key = "".join(str(1 - inst[i]) for inst in instances)
+            if key in seen or complement_key in seen:
+                continue
+            seen.add(key)
+
+        new_feature_names.append(feature_names[i])
+        for k in range(len(new_instances)):
+            new_instances[k].append(instances[k][i])
+
+    return new_feature_names, new_instances
+
+def turn_numeric(feature_names, instances):
+    new_feature_names = ["time", "event"]
+    new_instances = [j[:2] for j in instances]
+    new_feature_meanings = {}
+
+    converted_features_amount = 0
+    for i in range(2, len(instances[0])):
+        values = set([inst[i] for inst in instances])
+
+        # Check whether this variable is not a numeric variable
+        if any(type(j) == str for j in values):
+            values = sorted([str(j) for j in values])
+
+            # If there are only two options, comparing to one of them will be enough
+            if len(values) == 2:
+                values = [values[0]]
+
+            for option in values:
+                # Create binary variables using this option
+                for k in range(len(instances)):
+                    new_instances[k].append(int(instances[k][i] == option))
+
+                # Save data
+                new_feature_name = f"CatFeat{converted_features_amount}"
+                new_feature_names.append(new_feature_name)
+                new_feature_meanings[new_feature_name] = f"lambda x: x['{feature_names[i]}'] == '{option}'"
+                converted_features_amount += 1
+        else:
+            # Leave the variable for what it is
+            new_feature_names.append(feature_names[i])
+            for k in range(len(new_instances)):
+                new_instances[k].append(instances[k][i])
+
+    return new_feature_names, new_instances, new_feature_meanings
+
+def turn_binary(feature_names, instances):
+    new_feature_names = ["time", "event"]
+    new_instances = [j[:2] for j in instances]
+    new_feature_meanings = {}
+
+    converted_features_amount = 0
+    for i in range(2, len(instances[0])):
+        values = set([inst[i] for inst in instances])
+
+        # Check whether this variable is not a binary variable
+        if values - set([0, 1]):
+            values = sorted([j for j in values])
+
+            # Create a list of thresholds between values
+            thresholds = [(values[j] + values[j + 1]) / 2 for j in range(len(values) - 1)]
+            if len(thresholds) > MAX_BINARIZATIONS_PER_FEATURE:
+                # Reduce the amount of thresholds to a certain maximum
+                indices = [round(((j + 0.5) * (len(thresholds) - 1)) / MAX_BINARIZATIONS_PER_FEATURE) for j in range(MAX_BINARIZATIONS_PER_FEATURE)]
+                thresholds = [thresholds[j] for j in indices]
+
+            for threshold in thresholds:
+                # Create binary variables using this threshold
+                for k in range(len(instances)):
+                    new_instances[k].append(int(instances[k][i] > threshold))
+                
+                # Save data
+                new_feature_name = f"NumFeat{converted_features_amount}"
+                new_feature_names.append(new_feature_name)
+                new_feature_meanings[new_feature_name] = f"lambda x: x['{feature_names[i]}'] > {threshold}"
+                converted_features_amount += 1
+        else:
+            # Leave the variable for what it is
+            new_feature_names.append(feature_names[i])
+            for k in range(len(new_instances)):
+                new_instances[k].append(instances[k][i])
+
+    return new_feature_names, new_instances, new_feature_meanings
+
+if __name__ == "__main__":
+    for output_directory in [NUMERIC_DIRECTORY, BINARY_DIRECTORY]:
+        # Remove all previously converted datasets
+        if not os.path.exists(output_directory):
+            os.mkdir(output_directory)
+        for filename in files_in_directory(output_directory):
+            os.remove(f"{output_directory}/{filename}")
+        
+        # Create a folder for metadata
+        if os.path.exists(f"{output_directory}/feature_meanings"):
+            shutil.rmtree(f"{output_directory}/feature_meanings")
+        os.mkdir(f"{output_directory}/feature_meanings")
+
+    for input_filename in files_in_directory(ORIGINAL_DIRECTORY):
+        name = input_filename[:-4]
+
         instances = []
         feature_names = []
-        binary_instances = []
-        binary_feature_meanings = []
 
-        f = open(f"{INPUT_DIRECTORY}/{input_filename}")
-        lines = f.read().strip()
-        if "," not in lines:
-            lines = lines.replace(";", ",")
-        lines = lines.split("\n")
+        f = open(f"{ORIGINAL_DIRECTORY}/{input_filename}")
+        lines = f.read().strip().split("\n")
         f.close()
 
         feature_names = lines[0].split(",")
         for line in lines[1:]:
             inst = parse_line(line)
             instances.append(inst)
-            if inst[0] <= 0:
-                inst[0] = 1e-6
-            binary_instances.append([inst[0], int(int(inst[1]) == 1)])
 
-        seen_features = set(["0" * len(instances), "1" * len(instances)])
-        for i in range(2, len(instances[0])):
-            values = set()
-            for inst in instances:
-                values.add(inst[i])
+        # Convert to numeric instances
+        numeric_feature_names, numeric_instances, numeric_feature_meanings = turn_numeric(feature_names, instances)
+        numeric_feature_names, numeric_instances = remove_redundant_binary_features(numeric_feature_names, numeric_instances)
 
-            if any(type(j) == str for j in values):
-                values = sorted([str(j) for j in values])
-
-                for name in values:
-                    boolean_values = []
-                    for k in range(len(instances)):
-                        boolean_values.append(int(instances[k][i] == name))
-                    key = "".join(str(k) for k in boolean_values)
-                    complement_key = "".join(str(1 - k) for k in boolean_values)
-
-                    if key in seen_features or complement_key in seen_features:
-                        continue
-                    seen_features.add(key)
-
-                    for k in range(len(instances)):
-                        binary_instances[k].append(boolean_values[k])
-                    binary_feature_meanings.append(f"lambda x: x['{feature_names[i]}'] == '{name}'")
-            else:
-                has_nan = any(np.isnan(j) for j in values)
-                values = [float("nan")] * has_nan + sorted(j for j in values if not np.isnan(j))
-
-                nan_boolean_values = []
-                for k in range(len(instances)):
-                    nan_boolean_values.append(int(np.isnan(instances[k][i])))
-                key = "".join(str(k) for k in nan_boolean_values)
-
-                if key not in seen_features:
-                    seen_features.add(key)
-
-                    for k in range(len(instances)):
-                        binary_instances[k].append(nan_boolean_values[k])
-                    binary_feature_meanings.append(f"lambda x: np.isnan(x['{feature_names[i]}'])")
-
-                thresholds = [((values[j] + values[j + 1]) / 2, values[j + 1]) for j in range(len(values) - 1)]
-                if len(thresholds) > MAX_BINARIZATIONS_PER_FEATURE:
-                    indices = [round(((j + 0.5) * (len(thresholds) - 1)) / MAX_BINARIZATIONS_PER_FEATURE) for j in range(MAX_BINARIZATIONS_PER_FEATURE)]
-                    thresholds = [thresholds[j] for j in indices]
-
-                for threshold, ceil_value in thresholds:
-                    boolean_values = []
-                    for k in range(len(instances)):
-                        boolean_values.append(int(instances[k][i] > threshold))
-                    key = "".join(str(k) for k in boolean_values)
-
-                    if key in seen_features:
-                        continue
-                    seen_features.add(key)
-
-                    for k in range(len(instances)):
-                        binary_instances[k].append(boolean_values[k])
-                    binary_feature_meanings.append(f"lambda x: x['{feature_names[i]}'] > {threshold}")
-
-        name = input_filename[:-4]
-        f = open(f"{OUTPUT_DIRECTORY}/{name}_binary.txt", "w")
-        for bin_inst in binary_instances:
-            f.write(" ".join(str(j) for j in bin_inst))
+        # Write numeric instances
+        f = open(f"{NUMERIC_DIRECTORY}/{name}.txt", "w")
+        f.write(",".join(numeric_feature_names))
+        f.write("\n")
+        for num_inst in numeric_instances:
+            f.write(",".join(str(j) for j in num_inst))
             f.write("\n")
         f.close()
 
-        if not os.path.exists(METADATA_DIRECTORY):
-            os.mkdir(METADATA_DIRECTORY)
-
-        f = open(f"{METADATA_DIRECTORY}/{name}_features.txt", "w")
-        f.write("\n".join(str(j) for j in binary_feature_meanings))
+        # Write numeric feature meanings
+        f = open(f"{NUMERIC_DIRECTORY}/feature_meanings/{name}.txt", "w")
+        for key, value in numeric_feature_meanings.items():
+            f.write(f"{key} = {value}")
+            f.write("\n")
         f.close()
 
-        assert len(binary_feature_meanings) == len(binary_instances[0]) - 2
-        print(f"\033[35;1mBinarized {str(input_filename)+':':<50} {len(binary_instances)} instances, {len(binary_instances[0]) - 2} features\033[0m")
+        # Convert to binary instances
+        binary_feature_names, binary_instances, binary_feature_meanings = turn_binary(numeric_feature_names, numeric_instances)
+        binary_feature_names, binary_instances = remove_redundant_binary_features(binary_feature_names, binary_instances)
+        for key, value in numeric_feature_meanings.items():
+            binary_feature_meanings[key] = value
+
+        # Write binary instances
+        f = open(f"{BINARY_DIRECTORY}/{name}.txt", "w")
+        f.write(",".join(binary_feature_names))
+        f.write("\n")
+        for bin_inst in binary_instances:
+            f.write(",".join(str(j) for j in bin_inst))
+            f.write("\n")
+        f.close()
+
+        # Write binary feature meanings
+        f = open(f"{BINARY_DIRECTORY}/feature_meanings/{name}.txt", "w")
+        for key, value in binary_feature_meanings.items():
+            f.write(f"{key} = {value}")
+            f.write("\n")
+        f.close()
+
+        # Print progress
+        print(f"\033[35;1mConverted {str(input_filename)} ({len(binary_instances)} instances)\033[0m")
+        print(f"\033[34m  - Original    {len(instances[0]) - 2} features\033[0m")
+        print(f"\033[34m  - Numeric     {len(numeric_instances[0]) - 2} features\033[0m")
+        print(f"\033[34m  - Binary      {len(binary_instances[0]) - 2} features\033[0m")
 
     print("\033[32;1mDone!\033[0m")
